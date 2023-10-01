2521fe20d52795142da106573a4458ae68ac2cae
Mercy <mercywmuriithi.mm@gmail.com>
2023-10-01 22:01:51 +0300

    lab done

--- lib/models.py
+++ lib/models.py
@@ -1,15 +1,13 @@
+# many_to_many/models.py
+
 from sqlalchemy import create_engine, func
-from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, MetaData
+from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+engine = create_engine('sqlite:///many_to_many.db')
 
-Base = declarative_base(metadata=metadata)
-
+Base = declarative_base()
 
 game_user = Table(
     'game_users',
@@ -28,29 +26,13 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
-    reviews = relationship('Review', backref=backref('game'))
     users = relationship('User', secondary=game_user, back_populates='games')
-    
+    reviews = relationship('Review', backref=backref('game'), cascade='all, delete-orphan')
+
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
             f'title={self.title}, ' + \
             f'platform={self.platform})'
-
-class Review(Base):
-    __tablename__ = 'reviews'
-    id = Column(Integer(), primary_key=True)
-    score = Column(Integer())
-    comment = Column(String())
-
-    game_id = Column(Integer(), ForeignKey('games.id'))
-
-    user_id = Column(Integer(), ForeignKey('users.id'))
-
-    def __repr__(self):
-        return f'Review(id={self.id}, ' + \
-            f'score={self.score}, ' + \
-            f'game_id={self.game_id})'
-
 
 class User(Base):
     __tablename__ = 'users'
@@ -60,10 +42,26 @@ class User(Base):
     created_at = Column(DateTime(), server_default=func.now())
     updated_at = Column(DateTime(), onupdate=func.now())
 
-    reviews = relationship('Review', backref=backref('user'))
     games = relationship('Game', secondary=game_user, back_populates='users')
+    reviews = relationship('Review', backref=backref('user'), cascade='all, delete-orphan')
 
-    # don't forget your __repr__()!
     def __repr__(self):
         return f'User(id={self.id}, ' + \
             f'name={self.name})'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+
+    score = Column(Integer())
+    comment = Column(String())
+
+    game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
